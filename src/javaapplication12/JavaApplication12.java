7f51716a51cff94707ad621070c098f837454f47
USUARIO <USUARIO@pc1-210>
2018-04-04 21:30:27 -0500

    se borraron comentarios.

--- src/javaapplication12/JavaApplication12.java
+++ src/javaapplication12/JavaApplication12.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaapplication12;
 
-/**
- *
- * @author USUARIO
- */
+
 public class JavaApplication12 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        int numero;
+        numero = 1;
+        System.out.println("El numero es: "+ 1);
     }
     
 }
